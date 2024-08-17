e68f1f55567622418895bd322bf218e7de35b215
Christian Lucas <102526459+ChristianLucas029@users.noreply.github.com>
2024-08-17 14:29:25 -0300

    Update Item.java

--- src/one/digitalinnovation/gof/composite/Item.java
+++ src/one/digitalinnovation/gof/composite/Item.java
@@ -1,5 +1,40 @@
 package one.digitalinnovation.gof.composite;
 
-public class Item {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class Item {
+    public abstract void exibir();
+}
+
+class ProdutoSimples extends Item {
+    private String nome;
+
+    public ProdutoSimples(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void exibir() {
+        System.out.println(nome);
+    }
+}
+
+class Caixa extends Item {
+    private List<Item> itens = new ArrayList<>();
+
+    public void adicionarItem(Item item) {
+        itens.add(item);
+    }
+
+    public void removerItem(Item item) {
+        itens.remove(item);
+    }
+
+    @Override
+    public void exibir() {
+        for (Item item : itens) {
+            item.exibir();
+        }
+    }
 }
