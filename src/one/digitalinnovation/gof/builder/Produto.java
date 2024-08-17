d1b923acc3659363ccb150b3354441787407b973
Christian Lucas <102526459+ChristianLucas029@users.noreply.github.com>
2024-08-17 14:28:55 -0300

    Update Produto.java

--- src/one/digitalinnovation/gof/builder/Produto.java
+++ src/one/digitalinnovation/gof/builder/Produto.java
@@ -1,5 +1,51 @@
 package one.digitalinnovation.gof.builder;
 
 public class Produto {
+    private String nome;
+    private double preco;
+    private String descricao;
 
+    private Produto(ProdutoBuilder builder) {
+        this.nome = builder.nome;
+        this.preco = builder.preco;
+        this.descricao = builder.descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static class ProdutoBuilder {
+        private String nome;
+        private double preco;
+        private String descricao;
+
+        public ProdutoBuilder nome(String nome) {
+            this.nome = nome;
+            return this;
+        }
+
+        public ProdutoBuilder preco(double preco) {
+            this.preco = preco;
+            return this;
+        }
+
+        public ProdutoBuilder descricao(String descricao) {
+            this.descricao = descricao;
+            return this;
+        }
+
+        public Produto build() {
+            return new Produto(this);
+        }
+    }
 }
+
