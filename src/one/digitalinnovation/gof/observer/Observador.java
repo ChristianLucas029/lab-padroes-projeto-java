efe0a9ba6ccc85c325ffde4e9e206119c8e72b88
Christian Lucas <102526459+ChristianLucas029@users.noreply.github.com>
2024-08-17 14:30:23 -0300

    Update Observador.java

--- src/one/digitalinnovation/gof/observer/Observador.java
+++ src/one/digitalinnovation/gof/observer/Observador.java
@@ -1,5 +1,39 @@
 package one.digitalinnovation.gof.observer;
 
-public class Observador {
+import java.util.ArrayList;
+import java.util.List;
 
+interface Observador {
+    void atualizar(String mensagem);
+}
+
+class Cliente implements Observador {
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void atualizar(String mensagem) {
+        System.out.println(nome + " recebeu notificação: " + mensagem);
+    }
+}
+
+class Loja {
+    private List<Observador> clientes = new ArrayList<>();
+
+    public void adicionarCliente(Observador cliente) {
+        clientes.add(cliente);
+    }
+
+    public void removerCliente(Observador cliente) {
+        clientes.remove(cliente);
+    }
+
+    public void notificarClientes(String mensagem) {
+        for (Observador cliente : clientes) {
+            cliente.atualizar(mensagem);
+        }
+    }
 }
